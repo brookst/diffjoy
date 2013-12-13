40f4d4314de59105d124ab113b2393a08dc3daaf
Tim Brooks <brooks@cern.ch>
2013-12-13 16:54:18 +0000

    Update project name

--- src/usbconfig.h
+++ src/usbconfig.h
@@ -122,8 +122,8 @@
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'P','r','o','j','e','c','t','i','l','e','s'
-#define USB_CFG_VENDOR_NAME_LEN 11
+#define USB_CFG_VENDOR_NAME     'S','k','o','o','r','b'
+#define USB_CFG_VENDOR_NAME_LEN 6
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -132,14 +132,14 @@
  * obdev's free shared VID/PID pair. See the file USBID-License.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'A','e','t','h','e','r','s','e','n','s','e'
-#define USB_CFG_DEVICE_NAME_LEN 11
+#define USB_CFG_DEVICE_NAME     'D','i','f','f','j','o','y'
+#define USB_CFG_DEVICE_NAME_LEN 7
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USBID-License.txt before you assign a name if you
  * use a shared VID/PID.
  */
 #define USB_CFG_SERIAL_NUMBER   '0', '0', '1'
-#define USB_CFG_SERIAL_NUMBER_LEN   3 
+#define USB_CFG_SERIAL_NUMBER_LEN   3
 /* Same as above for the serial number. If you don't want a serial number,
  * undefine the macros.
  * It may be useful to provide the serial number through other means than at
